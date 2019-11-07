70f0d088b25000c3908b1aa4666ff99c5e34ae0f
vishalrjpt02 <55855196+vishalrjpt02@users.noreply.github.com>
2019-11-07 12:14:33 +0530

    Add files via upload

---
+++ Basic/test01.java
@@ -0,0 +1,21 @@
+class test01
+{
+  public static void main(String []args)
+  {
+   
+    String in= "Hello <<User Name>>, How are you?";
+    in=in.replace("<<User Name>>","vishal");
+    System.out.println(in);
+    
+	 /*
+		int num=Integer.parseInt(args[0]);
+		double out=0;
+		for(int i=0;i<=num;i++)
+		{
+			out=Math.pow(2,i);
+		}
+		System.out.println(out); */
+
+  }
+
+}
